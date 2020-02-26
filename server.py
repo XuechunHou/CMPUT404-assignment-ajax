944f12ef30851fc673768f81ece1d87557e6c4d6
XuechunHou <xhou1@ualberta.ca>
2020-02-26 00:42:06 -0700

    first submission

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, url_for, redirect, Response
 import json
 app = Flask(__name__)
 app.debug = True
@@ -64,37 +64,47 @@ myWorld = World()
 def flask_post_json():
     '''Ah the joys of frameworks! They do so much work for you
        that they get in the way of sane operation!'''
+    # print("what is request")
+    # print(request.form)
     if (request.json != None):
+        
         return request.json
     elif (request.data != None and request.data.decode("utf8") != u''):
+        
         return json.loads(request.data.decode("utf8"))
     else:
+
         return json.loads(request.form.keys()[0])
 
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect(url_for('static', filename='index.html'))
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
+    data = flask_post_json()
+    
+    for k,v in data.items():
+        myWorld.update(entity, k, v)
     '''update the entities via this interface'''
-    return None
+    return json.loads(json.dumps(myWorld.get(entity)))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.loads(json.dumps(myWorld.world()))
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.loads(json.dumps(myWorld.get(entity)))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.loads(json.dumps(myWorld.world()))
 
 if __name__ == "__main__":
     app.run()
